4aeedbf5de4715338c25071ae99ef9269e0683a1
visontail <the051425@gmail.com>
2023-11-10 11:27:55 +0100

    file structure

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # importing 'database' py file as 'db'
-import database as db
+import database.database as db
 # importing / using dotenv for storing login data locally
 import os
 from dotenv import load_dotenv
